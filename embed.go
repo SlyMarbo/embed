8710964150b2f4536a92163c3bff3173d9b4ea55
Jamie Hall <the.sly.marbo@googlemail.com>
2015-03-18 21:41:42 +0000

    Added package doc

--- embed.go
+++ embed.go
@@ -1,3 +1,13 @@
+// Command embed stores files as resources in Go source.
+//
+// Embed reads the input files and writes their contents as embedded data
+// in one Go file per input file, by appending .go to the filename.
+// Specifying -o overrides this by writing all files to a single output
+// with the given name. Embed attempts to detect the package name but
+// it can be specified with -package.
+//
+// 	$ embed -o content.go -gzip -sha1 content/index.html content/style.css
+//
 package main
 
 import (
